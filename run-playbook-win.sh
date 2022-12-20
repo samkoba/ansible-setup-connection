0cde3803e984cf99eb234b2c65a13c25352dec84
T.M FIKRI FEBRIANSYAH <hdct.antivirus@gmail.com>
2022-12-20 16:16:18 +0700

    init2

--- run-playbook-win.sh
+++ run-playbook-win.sh
@@ -1,1 +1,2 @@
-ansible-playbook -i inventory.ini check-connection-win.yml
+ansible-playbook -i inventory-win.ini check-connection-win.yml
+ansible-playbook -i inventory-linux.ini check-connection-linux.yml
